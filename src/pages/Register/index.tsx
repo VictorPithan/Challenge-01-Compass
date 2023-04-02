6c0bedbed2b9b3c1727c5c24966faa39c920c387
VictorPithan <victorpithan@gmail.com>
2023-04-01 21:05:09 -0300

    style:clean and standardized code

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,6 +1,12 @@
-import { z } from "zod"
-import { ChangeEvent, FormEvent, useState } from "react";
-import { Container, ContainerRegister, FormContainer, ImgBackground, InputForm } from "./styles";
+import { z } from 'zod'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import {
+  Container,
+  ContainerRegister,
+  FormContainer,
+  ImgBackground,
+  InputForm,
+} from './styles'
 
 import userIcon from '../../assets/user.svg'
 import lockIcon from '../../assets/lock-simple.svg'
@@ -10,24 +16,35 @@ import atIcon from '../../assets/atIcon.svg'
 import shieldCheckIcon from '../../assets/shieldCheckIcon.svg'
 import backgroundImg from '../../assets/sideImage.png'
 
-
-const registerFormSchema = z.object({
-  name: z.string().nonempty({message: "O campo nome é obrigatório"}),
-  userName: z.string().nonempty({message: "O campo usuário é obrigatório"}),
-  birthDate: z.date({required_error: "O campo data de nascimento é obrigatório", invalid_type_error: "Formato de dado inválido"}).min(new Date("1900-01-01"), {message: "Data inválida"}).max(new Date(), {message: "Data inválida"}),
-  email: z.string().email({message: "Formato de email inválido"}).min(1, {message: "O campo email é obrigatório"}),
-  password: z.string().nonempty({message: "O campo senha é obrigatório"}),
-  confirmPassword: z.string().nonempty({message: "O campo confirmar senha é obrigatório"}),
-})
-.superRefine(({ password, confirmPassword }, ctx) => {
-  if (password !== confirmPassword) {
-    ctx.addIssue({
-      code: "custom",
-      path: ["confirmPasswordInvalid"],
-      message: "As senhas não correspondem",
-    })
-  }
-})
+const registerFormSchema = z
+  .object({
+    name: z.string().nonempty({ message: 'O campo nome é obrigatório' }),
+    userName: z.string().nonempty({ message: 'O campo usuário é obrigatório' }),
+    birthDate: z
+      .date({
+        required_error: 'O campo data de nascimento é obrigatório',
+        invalid_type_error: 'Formato de dado inválido',
+      })
+      .min(new Date('1900-01-01'), { message: 'Data inválida' })
+      .max(new Date(), { message: 'Data inválida' }),
+    email: z
+      .string()
+      .email({ message: 'Formato de email inválido' })
+      .min(1, { message: 'O campo email é obrigatório' }),
+    password: z.string().nonempty({ message: 'O campo senha é obrigatório' }),
+    confirmPassword: z
+      .string()
+      .nonempty({ message: 'O campo confirmar senha é obrigatório' }),
+  })
+  .superRefine(({ password, confirmPassword }, ctx) => {
+    if (password !== confirmPassword) {
+      ctx.addIssue({
+        code: 'custom',
+        path: ['confirmPasswordInvalid'],
+        message: 'As senhas não correspondem',
+      })
+    }
+  })
 
 type registerFormInputs = z.infer<typeof registerFormSchema>
 
@@ -46,49 +63,54 @@ export function Register() {
   const [formErrors, setFormErrors] = useState<Record<string, string>>({})
 
   function handleValidateNameChange(event: ChangeEvent<HTMLInputElement>) {
-    event.target.setCustomValidity("");
+    event.target.setCustomValidity('')
     user.name = event.target.value
-    setUser({...user})
+    setUser({ ...user })
   }
 
   function handleValidateUserNameChange(event: ChangeEvent<HTMLInputElement>) {
-    event.target.setCustomValidity("");
+    event.target.setCustomValidity('')
     user.userName = event.target.value
-    setUser({...user})
+    setUser({ ...user })
   }
 
   function handleValidateBirthDateChange(event: ChangeEvent<HTMLInputElement>) {
-    event.target.setCustomValidity("");
+    event.target.setCustomValidity('')
     user.birthDate = new Date(event.target.value)
-    setUser({...user})
+    setUser({ ...user })
   }
 
   function handleValidateEmailChange(event: ChangeEvent<HTMLInputElement>) {
-    event.target.setCustomValidity("");
+    event.target.setCustomValidity('')
     user.email = event.target.value
-    setUser({...user})
+    setUser({ ...user })
   }
 
   function handleValidatePasswordChange(event: ChangeEvent<HTMLInputElement>) {
-    event.target.setCustomValidity("");
+    event.target.setCustomValidity('')
     user.password = event.target.value
-    setUser({...user})
+    setUser({ ...user })
   }
 
-  function handleValidateConfirmPasswordChange(event: ChangeEvent<HTMLInputElement>) {
-    event.target.setCustomValidity("");
+  function handleValidateConfirmPasswordChange(
+    event: ChangeEvent<HTMLInputElement>,
+  ) {
+    event.target.setCustomValidity('')
     user.confirmPassword = event.target.value
-    setUser({...user})
+    setUser({ ...user })
   }
 
   function handleValidateRegisterUser(event: FormEvent) {
     event.preventDefault()
-    
+
     const parsedUser = registerFormSchema.safeParse(user)
-    if(!parsedUser.success) {
+    if (!parsedUser.success) {
       const error = parsedUser.error
       let newErrors = {}
       for (const issue of error.issues) {
+        if (issue.message === 'Invalid date' && issue.path[0] === 'birthDate') {
+          issue.message = 'O campo data de nascimento é obrigatório'
+        }
         newErrors = {
           ...newErrors,
           [issue.path[0]]: issue.message,
@@ -98,24 +120,23 @@ export function Register() {
     }
 
     setUsers([...users, user])
-    
-    alert("Registro feito com sucesso")
+
+    alert('Registro feito com sucesso')
 
     setUser({
       name: '',
       userName: '',
-      birthDate: new Date(''),
+      birthDate: new Date(),
       email: '',
       password: '',
       confirmPassword: '',
     })
-    
-    
+
+    setTypeInput('text')
     return setFormErrors({})
   }
 
-  console.log("USER => ", user)
-  console.log("USERS => ", users)
+  const [typeInput, setTypeInput] = useState('text')
   return (
     <Container>
       <ContainerRegister>
@@ -127,12 +148,12 @@ export function Register() {
             <InputForm>
               <input
                 name="name"
-                type='text'
+                type="text"
                 placeholder="Nome"
                 value={user.name}
                 onChange={handleValidateNameChange}
                 className={
-                  Object.keys(formErrors)[0] === "name" ? 'invalid-input' : ''
+                  Object.keys(formErrors)[0] === 'name' ? 'invalid-input' : ''
                 }
               />
               <img src={userIcon} alt="" />
@@ -141,12 +162,14 @@ export function Register() {
             <InputForm>
               <input
                 name="userName"
-                type='text'
+                type="text"
                 placeholder="Usuário"
                 value={user.userName}
                 onChange={handleValidateUserNameChange}
                 className={
-                  Object.keys(formErrors)[0] === "userName" ? 'invalid-input' : ''
+                  Object.keys(formErrors)[0] === 'userName'
+                    ? 'invalid-input'
+                    : ''
                 }
               />
               <img src={fingerPrintIcon} alt="" />
@@ -155,11 +178,14 @@ export function Register() {
             <InputForm>
               <input
                 name="birthDate"
-                type='date'
+                type={typeInput}
+                onFocus={() => setTypeInput('date')}
                 placeholder="Nascimento"
                 onChange={handleValidateBirthDateChange}
                 className={
-                  Object.keys(formErrors)[0] === "birthDate" ? 'invalid-input' : ''
+                  Object.keys(formErrors)[0] === 'birthDate'
+                    ? 'invalid-input'
+                    : ''
                 }
               />
               <img src={cakeIcon} alt="" />
@@ -168,27 +194,29 @@ export function Register() {
             <InputForm>
               <input
                 name="email"
-                type='email'
+                type="email"
                 placeholder="Email"
                 value={user.email}
                 onChange={handleValidateEmailChange}
                 className={
-                  Object.keys(formErrors)[0] === "email" ? 'invalid-input' : ''
+                  Object.keys(formErrors)[0] === 'email' ? 'invalid-input' : ''
                 }
               />
               <img src={atIcon} alt="" />
             </InputForm>
-            
+
             <InputForm>
               <input
                 name="password"
-                type='password'
+                type="password"
                 placeholder="Senha"
                 value={user.password}
                 onChange={handleValidatePasswordChange}
                 className={
-                  Object.keys(formErrors)[0] === "password" ||
-                  Object.keys(formErrors)[0] === "confirmPasswordInvalid" ? 'invalid-input' : ''
+                  Object.keys(formErrors)[0] === 'password' ||
+                  Object.keys(formErrors)[0] === 'confirmPasswordInvalid'
+                    ? 'invalid-input'
+                    : ''
                 }
               />
               <img src={lockIcon} alt="" />
@@ -197,21 +225,29 @@ export function Register() {
             <InputForm>
               <input
                 name="confirmPassword"
-                type='password'
+                type="password"
                 placeholder="Confirmar senha"
                 value={user.confirmPassword}
                 onChange={handleValidateConfirmPasswordChange}
                 className={
-                  Object.keys(formErrors)[0] === "confirmPassword" ||
-                  Object.keys(formErrors)[0] === "confirmPasswordInvalid" ? 'invalid-input' : ''
+                  Object.keys(formErrors)[0] === 'confirmPassword' ||
+                  Object.keys(formErrors)[0] === 'confirmPasswordInvalid'
+                    ? 'invalid-input'
+                    : ''
                 }
               />
               <img src={shieldCheckIcon} alt="" />
             </InputForm>
-            { Object.keys(formErrors)[0] && <p className="invalidInput">{formErrors[Object.keys(formErrors)[0]]}</p> }
-            
+            {Object.keys(formErrors)[0] && (
+              <p className="invalidInput">
+                {formErrors[Object.keys(formErrors)[0]]}
+              </p>
+            )}
+
             <button type="submit">Registrar-se</button>
-            <span>Já possui uma conta? <a href="/">Faça Login</a></span>
+            <span>
+              Já possui uma conta? <a href="/">Faça Login</a>
+            </span>
           </fieldset>
         </FormContainer>
       </ContainerRegister>
