681251ade96f2195bb2cadb61691063be5d270c6
VictorPithan <victorpithan@gmail.com>
2023-04-27 16:19:01 -0300

    feat:adding routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express'
+import { users } from './storage/listUsers'
+import { listPosts } from './storage/listPosts'
 
 const routes = Router()
 
-routes.get('/', (req, res) => {
-  res.send('Funcionando')
+routes.get('/api/v1/user', (req, res) => {
+  res.send(users)
+})
+
+routes.get('/api/v1/user/post', (req, res) => {
+  res.send(listPosts)
 })
 
 export default routes
