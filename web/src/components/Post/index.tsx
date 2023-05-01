07330d616d46ebfafbd093f1ec62e9a119b79e8b
VictorPithan <victorpithan@gmail.com>
2023-04-30 22:42:28 -0300

    feat:adding comments

--- web/src/components/Post/index.tsx
+++ web/src/components/Post/index.tsx
@@ -1,38 +1,202 @@
-import { Avatar } from "../Avatar";
-import { BottomButtonsContainer, ImgPost, PostContainer } from "./styles";
+import { ChangeEvent, FormEvent, InvalidEvent, useEffect, useState } from "react";
 
-import ilhaDoComendador from '../../assets/ilhaDoComendador.png'
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
+import { Avatar } from "../Avatar";
+import { BottomButtonsContainer, Comment, ImgPost, NewCommentPost, PostButton, PostContainer } from "./styles";
+
 import likeIcon from '../../assets/likeIcon.svg'
 import commentIcon from '../../assets/commentIcon.svg'
 import shareIcon from '../../assets/shareIcon.svg'
 import oClockIcon from '../../assets/oClockIcon.svg'
 
-export function Post() {
+import cameraIcon from '../../assets/cameraIcon.svg'
+import imageIcon from '../../assets/imageIcon.svg'
+import attachmentIcon from '../../assets/attachmentIcon.svg'
+import mapPinIcon from '../../assets/mapPinIcon.svg'
+import emojiIcon from '../../assets/emojiIcon.svg'
+import iconProfile from '../../assets/profileIcon.jpg'
+
+// interface AuthorPost {
+//   user: string;
+//   avatarURL: string;
+// }
+
+interface CommentProps {
+  user: string;
+  avatarURL?: string;
+  comment: string;
+}
+
+interface PostProps {
+  user: string;
+  userName?: string;
+  avatarURL?: string;
+  publishedAt: Date | string;
+  description: string;
+  likes: number;
+  imagePost?: string | null;
+  comments: CommentProps[];
+}
+
+export function Post({user, userName, avatarURL = iconProfile, publishedAt, description, likes, comments = [], imagePost = null}: PostProps) {
+  const [listComments, setListComments] = useState(comments)
+
+  const [newCommentText, setNewCommentText] = useState("")
+
+  function handleCreateNewComment(event: FormEvent) {
+    event.preventDefault()
+
+    const newComment = {
+      user,
+      comment: newCommentText
+    }
+
+    setListComments([newComment, ...listComments])
+    setNewCommentText("")
+  }
+
+  function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    event.target.setCustomValidity("")
+    setNewCommentText(event.target.value)
+  }
+
+  function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
+    event.target.setCustomValidity("Esse campo é obrigatório!")
+  }
+
+  const publishedDateFormatted = format(
+    new Date(publishedAt),
+    "d 'de' LLLL 'às' HH:mm'h'",
+    {
+      locale: ptBR,
+    }
+  )
+
+  const publishedDateRelativeToNow = formatDistanceToNow(new Date(publishedAt), {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
+  const isNewCommentEmpty = newCommentText.length === 0
+
   return (
     <PostContainer>
       <header>
         <div>
-          <Avatar src="https://github.com/victorpithan.png" alt="" width={32} height={32}/>
+          <Avatar src={avatarURL} alt="" width={32} height={32}/>
           <div>
-            <p>Victor Pithan</p>
+            <p>{user}</p>
             <time
-              title="25-04-2023"
-              dateTime="25-04-2023"
+              title={publishedDateFormatted}
+              dateTime={new Date(publishedAt).toISOString()}
             >
               <img src={oClockIcon} alt="" />
-              12 minutos atrás em <strong>paisagens exuberantes</strong>
+              {publishedDateRelativeToNow} atrás em <strong>paisagens exuberantes</strong>
             </time>
           </div>
         </div>
       </header>
-      <p>Minha última viagem para a ilha do Comendador, um lugar simplesmente incrível, natureza praticamente intocada. Recomendo a todos que apreciam o mundo como ele é.</p>
-      <ImgPost src={ilhaDoComendador} alt="ilha do comendador" />
+      <p>{description}</p>
+      {imagePost != null ? <ImgPost src={imagePost} alt="" /> : ''}
 
       <BottomButtonsContainer>
-        <button><img src={likeIcon} alt="" /> Curtiu <span>1.7k</span></button>
-        <button><img src={commentIcon} alt="" /> Comentários <span>345</span></button>
+        <button><img src={likeIcon} alt="" /> Curtiu <span>{likes}</span></button>
+        <button><img src={commentIcon} alt="" /> Comentários <span>{comments.length}</span></button>
         <button><img src={shareIcon} alt="" /> Compartilhar</button>
       </BottomButtonsContainer>
+
+      <NewCommentPost>
+        <Avatar src="https://github.com/victorpithan.png" width={32} height={32}/>
+        <form onSubmit={handleCreateNewComment} className="commentForm">
+          <div>
+            <textarea
+              className="textarea-style"
+              name="comment"
+              placeholder="O que voce está pensando?"
+              value={newCommentText}
+              onChange={handleNewCommentChange}
+              onInvalid={handleNewCommentInvalid}
+              required
+            />
+            <ul className="bottom-button-hidden">
+              <li>
+                <button>
+                  <img src={cameraIcon} alt="" />
+                </button>
+              </li>
+              <li>
+                <button>
+                  <img src={imageIcon} alt="" />
+                </button>
+              </li>
+              <li>
+                <button>
+                  <img src={attachmentIcon} alt="" />
+                </button>
+              </li>
+              <li>
+                <button>
+                  <img src={mapPinIcon} alt="" />
+                </button>
+              </li>
+              <li>
+                <button>
+                  <img src={emojiIcon} alt="" />
+                </button>
+              </li> 
+            </ul>
+          </div>
+          <footer>
+            <ul>
+              <li>
+                <button disabled>
+                  <img src={cameraIcon} alt="" />
+                </button>
+              </li>
+              <li>
+                <button disabled>
+                  <img src={imageIcon} alt="" />
+                </button>
+              </li>
+              <li>
+                <button disabled>
+                  <img src={attachmentIcon} alt="" />
+                </button>
+              </li>
+              <li>
+                <button disabled>
+                  <img src={mapPinIcon} alt="" />
+                </button>
+              </li>
+              <li>
+                <button disabled>
+                  <img src={emojiIcon} alt="" />
+                </button>
+              </li> 
+            </ul>
+            <PostButton type="submit" disabled={isNewCommentEmpty}>
+              Postar
+            </PostButton>
+          </footer>
+          
+        </form>
+      </NewCommentPost>
+      <p>Todos os comentários</p>
+      {
+        listComments.map(comment => {
+          return (
+            <Comment key={`${comment.comment}${comment.user}`}>
+              <Avatar src={comment.avatarURL = iconProfile} width={24} height={24}/>
+              <p><span>{comment.user}: </span>{comment.comment}</p>
+            </Comment>
+          )
+        })
+      }
+
+      <hr />
+      <p className="allComments">Ver todos os comentários</p>
     </PostContainer>
   )
 }
