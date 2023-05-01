22573cb06d8d9ea76accb1d8e195697867d646ac
VictorPithan <victorpithan@gmail.com>
2023-04-30 22:41:10 -0300

    feat:updating styles

--- web/src/pages/Home/components/Main/styles.ts
+++ web/src/pages/Home/components/Main/styles.ts
@@ -63,12 +63,13 @@ export const BoxTopicContainer = styled.div`
   border-radius: 16px;
   border: 2px solid ${props => props.theme["gray-600"]};
   padding: 1.5rem 1.25rem;
+  overflow: auto;
 
   p {
     font-family: 'Inter', sans-serif;
     line-height: 1.1875;
 
-    & + div {
+    & + ul {
       margin-top: 1.5rem;
     }
   }
@@ -77,18 +78,109 @@ export const BoxTopicContainer = styled.div`
     margin-top: 2.25rem;
   }
 
-  div {
+  li {
     display: flex;
     align-items: center;
     gap: 1rem;
+    text-decoration: none;
 
     span {
       font-size: 0.875rem;
       line-height: 1.5;
     }
 
-    & + div {
+    & + li {
       margin-top: 1rem;
     }
   }
+
+  &::-webkit-scrollbar {
+    
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 50px;
+    background: ${props => props.theme["gray-600"]};
+  }
+
+  &::-webkit-scrollbar-track {
+    display: none;
+    background: ${props => props.theme["gray-700"]};
+  }
+`;
+
+export const NewPost = styled.div`
+  background: ${props => props.theme["gray-700"]};
+  border-radius: 16px;
+  border: 2px solid ${props => props.theme["gray-600"]};
+  padding: 1rem;
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+
+  textarea {
+    width: 100%;
+    height: 2rem;
+    border: none;
+    border-radius: 16px;
+    resize: none;
+    background: ${props => props.theme["gray-650"]};
+    padding-inline: 1.5rem;
+    padding-top: 0.3rem;
+    padding-right: 12rem;
+    color: ${props => props.theme.white};
+    font-size: 0.875rem;
+    line-height: 1.5;
+    overflow-y: hidden;
+
+    &::placeholder {
+      color: ${props => props.theme["gray-300"]};
+      font-size: 0.875rem;
+      line-height: 1.5;
+    }
+  }
+`;
+
+export const BottomButtons = styled.div`
+  display: flex;
+  gap: 1.5rem;
+  align-items: center;
+  button { 
+    background: transparent;
+    border: none;
+    cursor: pointer; 
+  }
+`;
+
+export const PostButton = styled.button`
+  background: transparent;
+  border: 2px solid ${props => props.theme["gray-650"]};
+  font-size: 0.875rem;
+  line-height: 1.5;
+  color: ${props => props.theme.white};
+  border-radius: 8px;
+  padding: 0.375rem 2rem;
+  cursor: pointer;
+
+  transition: filter 0.2s;
+
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
+
+  &:not(:disabled):hover {
+    filter: brightness(0.9);
+  }
+`;
+
+export const BottomButtonsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 1.125rem;
 `;
