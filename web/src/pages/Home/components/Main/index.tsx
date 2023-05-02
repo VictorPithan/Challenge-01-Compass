2260916931be991f2bd4c47cd52d0117e0ce2a07
VictorPithan <victorpithan@gmail.com>
2023-05-02 01:31:26 -0300

    refactor:remove some function to useContext

--- web/src/pages/Home/components/Main/index.tsx
+++ web/src/pages/Home/components/Main/index.tsx
@@ -1,10 +1,9 @@
-import { ChangeEvent, FormEvent, InvalidEvent, useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, InvalidEvent, useState, useContext } from "react";
 
 import { BodyContainer, BottomButtons, BottomButtonsContainer, BoxTopicContainer, Container, Header, HeaderContainer, MenuContainer, NewPost, PostButton, PostsContainer, Profile } from "./styles";
 
 import { Post } from "../../../../components/Post";
 import { Avatar } from "../../../../components/Avatar";
-import { BoxToCreateNewPost } from "../../../../components/BoxToCreateNewPost";
 import { TopicContainer } from "./styles";
 
 import homeIcon from '../../../../assets/home.svg'
@@ -14,107 +13,28 @@ import attachmentIcon from '../../../../assets/attachmentIcon.svg'
 import mapPinIcon from '../../../../assets/mapPinIcon.svg'
 import emojiIcon from '../../../../assets/emojiIcon.svg'
 import profileIcon from '../../../../assets/profileIcon.jpg'
-import ilhaDoComendador from '../../../../assets/ilhaDoComendador.png'
+import { DataContext } from "../../../../contexts/DataContext";
 
-interface PostProps {
-  user: string;
-  name?: string;
-  profile_photo?: string;
-  post_date: Date | string;
+interface CreatePostInput {
   description: string;
-  likes: number;
-  comments: CommentProps[];
-  url_imagem?: string;
-}
-
-interface CommentProps {
-  user: string;
-  name: string;
-  profile_photo: string;
-  comment: string;
-}
-
-interface usersProps {
-  name: string;
-  user: string;
-  birthdate: string;
-  email: string;
-  password: string;
-  profile_photo: string;
-}
-
-const userAuthenticated = {
-  user: "victor pithan 2",
-  name: "Victor Pithan",
-  url_imagem: "https://github.com/victorpithan.png",
+  url_imagem: string;
 }
 
 export function Main() {
 
-  const [posts, setPosts] = useState<PostProps[]>([
-    {
-      user: "victorpithan",
-      name: "Victor Pithan",
-      profile_photo: "https://github.com/victorpithan.png",
-      post_date: new Date("2023-04-28T16:15:00"),
-      description: "Minha última viagem para a ilha do Comendador, um lugar simplesmente incrível, natureza praticamente intocada. Recomendo a todos que apreciam o mundo como ele é.",
-      likes: 100,
-      comments: [
-        {
-          "user": "diegosuarez",
-          "name": "Diego Suarez",
-          "profile_photo": "",
-          "comment": "Linda foto Victor! 🌸"
-        },
-        {
-          "user": "luizbrugnera",
-          "name": "Luiz Brugnera",
-          "profile_photo": "",
-          "comment": "Que praia maravilhosa! 🌷"
-        }
-      ],
-      url_imagem: ilhaDoComendador
-    },
-  ])
-
+  const { createPost, posts, users, userlogged } = useContext(DataContext)
   const [newPostText, setNewPostText] = useState("")
 
-  const [users, setUsers] = useState<usersProps[]>([])
-
-  useEffect(() => {
-    fetch('http://localhost:3333/api/v1/user/post')
-      .then(response => response.json())
-      .then(data => {
-        // const newPosts: PostProps[] = data.posts
-        // console.log(data);
-        setPosts([...posts, ...data])
-      })
-
-    fetch('http://localhost:3333/api/v1/user')
-      .then(response => response.json())
-      .then(data => {
-        const newUsers: usersProps[] = data.users
-        setUsers([...users, ...newUsers])
-      })
-  }, [])
-
-  const imagePost = ilhaDoComendador //"https://picsum.photos/710/300"
-
-  function handleCreateNewPost(event: FormEvent) {
+  async function handleCreateNewPost(event: FormEvent) {
     event.preventDefault()
 
-    const newPost = {      
-      user: userAuthenticated.user,
-      name: userAuthenticated.name,
-      profile_photo: userAuthenticated.url_imagem,
-      post_date: new Date(),
+    const newPost: CreatePostInput = {
       description: newPostText,
-      likes: 0,
-      comments: [],
-      url_imagem: "",
+      url_imagem: ""
     }
+    
+    await createPost(newPost)
 
-    setPosts([newPost, ...posts])
     setNewPostText("")
   }
 
@@ -137,8 +57,8 @@ export function Main() {
           <span>Home</span>
         </MenuContainer>
         <Profile>
-          <span>Victor Pithan</span>
-          <Avatar src="https://github.com/victorpithan.png" alt="" width={42} height={42}/>
+          <span>{userlogged?.name}</span>
+          <Avatar src={userlogged?.profile_photo ?? profileIcon} alt="" width={42} height={42}/>
         </Profile>        
       </Header>
 
@@ -147,7 +67,7 @@ export function Main() {
           <NewPost>
             <form onSubmit={handleCreateNewPost}>
               <HeaderContainer>
-                <Avatar src={userAuthenticated.url_imagem} alt="" width={32} height={32}/>
+                <Avatar src={userlogged?.profile_photo ?? profileIcon} alt="" width={32} height={32}/>
                 <textarea
                   name="post"
                   placeholder="No que você está pensando?"
@@ -172,18 +92,18 @@ export function Main() {
             </form>
           </NewPost>
 
-          {posts.map((post) => {
+          {posts.map((post, i) => {
             return (
               <Post
                 userName={post?.name}
                 user={post?.user}
-                avatarURL={post?.profile_photo}
+                avatarURL={post?.profile_photo ?? profileIcon}
                 publishedAt={post?.post_date}
                 description={post?.description}
                 likes={post?.likes}
                 comments={post?.comments}
                 imagePost={post?.url_imagem}
-                key={`${post?.description}${post?.user}`}
+                key={`${i}${post.description}`}
               />
             )
           })}
@@ -193,10 +113,10 @@ export function Main() {
           <BoxTopicContainer>
             <p>Meus Amigos</p>
             <ul>
-              {users.map((user) => {
+              {users.map((user, i) => {
                 return (
-                <li key={user.user}>
-                  <Avatar src={user.profile_photo = profileIcon} alt="" width={32} height={32}/>
+                <li key={i}>
+                  <Avatar src={user.profile_photo ?? profileIcon} alt="" width={32} height={32}/>
                   <span>{user.name}</span>
                 </li>
               )})}
