a4554cdcd97568cf4c124b482f578fa4bb94f1a2
VictorPithan <victorpithan@gmail.com>
2023-05-28 19:49:59 -0300

    feat: add additional entity configs

--- api/src/database/configs/data-source.config.ts
+++ api/src/database/configs/data-source.config.ts
@@ -1,11 +1,14 @@
 import { join } from 'path';
+import { Comment } from 'src/posts/entities/comment.entity';
+import { Post } from 'src/posts/entities/post.entity';
+import { User } from 'src/users/entities/user.entity';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'sqlite',
   database: 'social-media',
   synchronize: true,
-  entities: [],
+  entities: [User, Post, Comment],
   migrations: [
     join(__dirname, '..', '..', '**', 'database/migrations/*.{ts,js}')
   ]
