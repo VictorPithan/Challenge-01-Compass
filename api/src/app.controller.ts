6aa0a9dc6066fb5463edd4ae85fa4de151ef98ba
VictorPithan <victorpithan@gmail.com>
2023-05-28 19:52:14 -0300

    feat: add small adjustments

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
 
-@Controller()
+@ApiTags('Api Infos')
+@Controller({ path: 'api', version: '1' })
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getInfos() {
+    return {
+      environment: process.env.ENV,
+      version: process.env.VERSION
+    };
   }
 }
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,20 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from './database/configs/data-source.config';
 import { DataSource } from 'typeorm';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(dataSourceOptions)
+    TypeOrmModule.forRoot(dataSourceOptions),
+    UsersModule,
+    AuthModule,
+    PostsModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {
   constructor(private dataSource: DataSource) {}
 }
-
